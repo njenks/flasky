e801d4b6efed322009915fc2cca3a09c86666b21
Natascha <njenkns@gmail.com>
2022-05-05 11:31:52 -0700

    got flasky to work again by going to whales github

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from dotenv import load_dotenv # function that loads dotenv ****
-import os # import *****
+from dotenv import load_dotenv # function that loads dotenv
+import os 
 
 db = SQLAlchemy() # creating new DB obj - we can import db into other files because outside of method
 migrate = Migrate() # creating new migrate obj
 load_dotenv()
 
-def create_app(testing = False): # ****
+def create_app(testing = None): 
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
-    if testing == {"testing": True}:
+    if testing is None:
         # telling sqlalchemy where our db is located
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_TESTING_DATABASE_URI') # *****
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     else: 
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')# *****
+        app.config['TESTING'] = True 
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_TESTING_DATABASE_URI') 
 
     db.init_app(app) # hooking up db and app - connecting objs to flask server
     migrate.init_app(app, db) # hooking up migrate and app - connecting objs to flask server
--- app/routes/cars.py
+++ app/routes/cars.py
@@ -1,138 +1,106 @@
-from flask import Blueprint, jsonify, request 
-from app import db 
+from flask import Blueprint, jsonify, request, make_response
+
+from app import db
 from app.models.cars import Car
 
-# class Car:
-#     def __init__(self, id, driver, team, mass_kg):
-#         self.id = id 
-#         self.driver = driver
-#         self.team = team
-#         self.mass_kg= mass_kg  
-
-# cars = [
-#     Car(7, "Sainz", "Ferrari", 795),
-#     Car(88, "Sharles", "Ferrari", 800),
-#     Car(4, "Danny Ric", "McLaren", 1138)
-# ]
-
-cars_bp = Blueprint("cars", __name__, url_prefix="/cars") # create a Blueprint obj - instantiating a class 
-# first param - names Blueprint, second param - helping flask do it's magic, third param - what we want our route to start with 
+cars_bp = Blueprint("cars", __name__, url_prefix="/cars")
 
 @cars_bp.route("", methods=["POST"])
-def create_car(): 
+def create_car():
     request_body = request.get_json()
 
     new_car = Car(
-        driver=request_body["driver"], 
-        team=request_body["team"], 
-        mass_kg=request["mass_kg"]
-    ) 
+        driver=request_body["driver"],
+        team=request_body["team"],
+        mass_kg=request_body["mass_kg"]
+    )
 
     db.session.add(new_car)
-    db.session.commit() # need this to save into db session 
+    db.session.commit()
 
-    return { 
-        "id":new_car.id
-    }, 201              
+    return {
+        "id": new_car.id
+    }, 201
 
-
-@cars_bp.route("", methods=["GET"]) # can add more methods if need be 
-# decorator changes what def get_all_cars() does 
+@cars_bp.route("", methods=["GET"])
 def get_all_cars():
-    params = request.args # can use anything as param - obj attr 
-    if "driver" in params and "team" in params: 
-        driver_query = params["driver"]
-        team_query = params["team"]
-        cars = Car.query.filter_by(driver=driver_query, team=team_query)
-
-    if "driver" in params: 
-        driver_query = params["driver"]
-        cars = Car.query.filter_by(driver=driver_query)
-        driver_query = request.args.get("driver")
-    elif "team" in params:
-        team_query = params["team"]
-        cars = Car.query.filter_by(team=team_query)
-        team_query = request.args.get("team")
-    else: 
-        cars = Car.query.all() 
-    # if driver_query:
-    #     cars = Car.query.filter_by(driver=driver_query)
-    # else:
-    #     cars = Car.query.all()
     response = []
-    for car in cars: 
+    cars = Car.query.all()
+    for car in cars:
         response.append(
             {
-                "id": car.id, 
+                "id": car.id,
                 "driver": car.driver,
-                "team": car.team, 
-                "mass_kg": car.mass_kg 
+                "team": car.team,
+                "mass_kg": car.mass_kg
             }
         )
-    return jsonify(response) # converts list of dicts into JSON list of JSON obj - response that goes back to client 
-    # with lists, explicity have to use jsonify to return JSON obj  - good idea to always use it, just in case 
-
+    return jsonify(response)
 
 @cars_bp.route("/<car_id>", methods=["GET"])
-def get_one_car(car_id): # making var that needs same exact name as route 
-    try: 
-        car_id = int(car_id) 
-    except ValueError: 
-        return jsonify({"msg":f"car {car_id} invalid. ID must be an integer"}, 400)
-    
+def get_one_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({'msg': f"Invalid car id: '{car_id}'. ID must be an integer"}), 400
+
     chosen_car = Car.query.get(car_id)
 
-    if chosen_car is None: 
-        return jsonify({"msg":f"car {car_id} not found"}, 404)
-
+    if chosen_car is None:
+        return jsonify({'msg': f'Could not find car with id {car_id}'}), 404 
+            
     return jsonify({
-                "id": chosen_car.id, 
-                "driver": chosen_car.driver, 
-                "team": chosen_car.team, 
-                "mass_kg": chosen_car.mass_kg
-            }), 200 # returns status code as well 
-
-
-@cars_bp.route("/<car_id>", method=["PUT"])
-def update_car(car_id): 
-    try: 
-        car_id = int(car_id) 
-    except ValueError: 
-        return jsonify({"msg":f"car {car_id} invalid. ID must be an integer"}, 400)
+        "id": chosen_car.id,
+        "driver": chosen_car.driver,
+        "team": chosen_car.team,
+        "mass_kg": chosen_car.mass_kg
+    })
     
-    request_body = request.get_json() # dict with k, v that were in JSON
+@cars_bp.route("/<car_id>", methods=["PUT"])
+def replace_one_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({'msg': f"Invalid car id: '{car_id}'. ID must be an integer"}), 400
+
+    request_body = request.get_json()
 
     if "driver" not in request_body or \
-        "team" not in request_body or \
-        "mass_kg" not in request_body: 
-        return jsonify({'msg': f"Request must include driver, team, and mass_kg"}), 400 
+    "team" not in request_body or \
+    "mass_kg" not in request_body:
+        return jsonify({'msg': f"Request must include driver, team, and mass_kg"}), 400
 
     chosen_car = Car.query.get(car_id)
 
-    if chosen_car is None: 
-        return jsonify({"msg":f"car {car_id} not found"}, 404)
+    if chosen_car is None:
+        return jsonify({'msg': f'Could not find car with id {car_id}'}), 404
 
     chosen_car.driver = request_body["driver"]
     chosen_car.team = request_body["team"]
     chosen_car.mass_kg = request_body["mass_kg"]
 
-    db.session.commit() 
+    db.session.commit()
 
-    return jsonify({'msg': f"Successfully replaced car with id {car_id}"}), 200 
+    return make_response(
+        jsonify({'msg': f"Successfully replaced car with id {car_id}"}),
+        200
+    )
 
-@cars_bp.route("/<car_id>", method=["DELETE"])
-def delete_car(car_id):
-    try: 
-        car_id = int(car_id) 
-    except ValueError: 
-        return jsonify({"msg":f"car {car_id} invalid. ID must be an integer"}, 400)
-    
+
+@cars_bp.route("/<car_id>", methods=["DELETE"])
+def delete_one_car(car_id):
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({'msg': f"Invalid car id: '{car_id}'. ID must be an integer"}), 400
+
     chosen_car = Car.query.get(car_id)
 
-    if chosen_car is None: 
-        return jsonify({"msg":f"car {car_id} not found"}, 404)
-    
-    db.session.delete(chosen_car)
-    db.session.commit() # any time making changes you want to save 
+    if chosen_car is None:
+        return jsonify({'msg': f'Could not find car with id {car_id}'}), 404
 
-    return jsonify({'msg': f"Car #{car_id} successfully deleted"})
+    db.session.delete(chosen_car)
+    db.session.commit()
+
+    return jsonify({'msg': f'Deleted car with id {car_id}'})
+
