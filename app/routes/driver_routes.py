eb99a612ee789f0c3e1a47db06eab09224687dca
Natascha <njenkns@gmail.com>
2022-05-11 10:07:28 -0700

    getting setup for deployment

--- app/routes/driver_routes.py
+++ app/routes/driver_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, make_response
 
 from app import db
 from app.models.drivers import Driver
+from app.models.cars import Car
 
 drivers_bp = Blueprint("drivers", __name__, url_prefix="/drivers")
 
@@ -37,12 +38,12 @@ def validate_driver(driver_id):
     try:
         driver_id = int(driver_id)
     except ValueError:
-        return jsonify({'msg': f"Invalid car id: '{driver_id}'. ID must be an integer"}), 400
+        return jsonify({'msg': f"Invalid driver id: '{driver_id}'. ID must be an integer"}), 400
 
     chosen_driver = Driver.query.get(driver_id)
 
     if chosen_driver is None:
-        return jsonify({'msg': f'Could not find car with id {driver_id}'}), 404 
+        return jsonify({'msg': f'Could not find driver with id {driver_id}'}), 404 
     
     return chosen_driver 
 
@@ -51,3 +52,40 @@ def get_one_driver(driver_id):
     driver = validate_driver(driver_id)
 
     return jsonify(driver.to_dict()), 200
+
+def validate_car(car_id): 
+    try:
+        car_id = int(car_id)
+    except ValueError:
+        return jsonify({'msg': f"Invalid car id: '{car_id}'. ID must be an integer"}), 400
+
+    chosen_car = Car.query.get(car_id)
+
+    if chosen_car is None:
+        return jsonify({'msg': f'Could not find car with id {car_id}'}), 404 
+    
+    return chosen_car 
+
+@drivers_bp.route("/<driver_id>/cars", methods=["POST"])
+def add_cars_to_driver(driver_id):
+    driver = validate_driver(driver_id)
+
+    request_body = request.get_json()
+    try:
+        car_ids = request_body
+    except KeyError: 
+        return jsonify({'msg': f"Missing car_ids in request body"}), 400 # missing key in k: v pair 
+
+    if not isinstance(car_ids, list): 
+        jsonify({'msg': f"Expected list of car ids"}), 400
+    
+    cars = []
+    for id in car_ids:  # checking to see if car_ids are incorrect 
+        cars.append(validate_car(id))
+
+    for car in cars: 
+        car.driver = driver 
+    
+    db.session.commit()
+
+    return jsonify({f'msg': "Added cars to driver {driver_id}"}), 200 
